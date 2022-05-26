9cbcff2bfbbd48570140acc76e90f4842ab479c5
igrek51 <igrek51.dev@gmail.com>
2022-05-26 17:33:28 +0200

    Enhance displaying the table

--- static/schedulio.new.js
+++ static/schedulio.new.js
@@ -5,109 +5,147 @@ const data = [
     ['Wed 2022-05-25', 'ok', '21-24', 'no', ''],
     ['Thu 2022-05-26', '', '', '', ''],
     ['Fri 2022-05-27', '', '', '', ''],
-];
+]
   
 function celRenderer(instance, td, row, col, prop, value, cellProperties) {
-    Handsontable.renderers.TextRenderer.apply(this, arguments);
+    Handsontable.renderers.TextRenderer.apply(this, arguments)
     if (row == 0) {
-        td.style.color = '#000000';
-        td.style.background = '#F8F9FA';
-        td.style.fontWeight = 'bold';
+        td.style.color = '#000000'
+        td.style.background = '#F8F9FA'
+        td.style.fontWeight = 'bold'
     } else if (col == 0) {
-        td.style.color = '#000000';
-        td.style.background = '#FFFFFF';
+        td.style.color = '#000000'
+        td.style.background = '#FFFFFF'
     } else if (value == 'ok') {
-        td.style.color = '#38782E';
-        td.style.background = '#D9EAD3';
-        td.style.fontWeight = 'bold';
+        td.style.color = '#38782E'
+        td.style.background = '#D9EAD3'
+        td.style.fontWeight = 'bold'
     } else if (value == 'no') {
-        td.style.color = '#AC322C';
-        td.style.background = '#EA9999';
-    } else if (value == '') {
-        td.style.color = '#000000';
-        td.style.background = '#FFFFFF';
+        td.style.color = '#AC322C'
+        td.style.background = '#EA9999'
+    } else if (value == '' || value == null || value == undefined) {
+        td.style.color = '#000000'
+        td.style.background = '#FFFFFF'
     } else {
-        td.style.color = '#000000';
-        td.style.background = '#FFF2CC';
-        td.style.fontWeight = 'bold';
+        td.style.color = '#000000'
+        td.style.background = '#FFF2CC'
+        td.style.fontWeight = 'bold'
     }
 }
-  
-const container = document.getElementById('scheduleTable');
+
+var loadMoreData = function(n) {
+    for (let i = 0; i < n; i += 1) {
+        data.push(['Sat 2022-05-27', '', '', '', ''])
+    }
+    hot.updateData(data)
+    hot.render()
+}
+
+const container = document.getElementById('scheduleTable')
 const hot = new Handsontable(container, {
     data: data,
     rowHeaders: false,
     colHeaders: false,
-    height: 'auto',
+    width: 'auto',
+    height: '20vw',
     colWidths(index) {
         if (index == 0) {
-            return 150;
+            return 150
         }
-        return 100;
+        return 100
     },
+    fixedRowsTop: 1,
     rowHeights: 40,
     className: 'htCenter htMiddle',
     manualColumnResize: true,
-    contextMenu: [
-        'ok',
-        'ok, but...',
-        '(maybe)',
-        'no',
-    ],
     cells(row, col) {
-        const cellProperties = {};
+        const cellProperties = {}
         if (col == 0) {
-            cellProperties.readOnly = true;
+            cellProperties.readOnly = true
         } else {
-            cellProperties.readOnly = false;
+            cellProperties.readOnly = false
         }
-        cellProperties.renderer = celRenderer;
-        return cellProperties;
+        cellProperties.renderer = celRenderer
+        return cellProperties
+    },
+    contextMenu: {
+        items: {
+            vote_ok: {
+                name: 'ok',
+                callback(key, selection, clickEvent) {
+                    setSelectedCells('ok')
+                }
+            },
+            vote_ok_hours: {
+                name: 'ok with hours',
+                callback(key, selection, clickEvent) {
+                    voteOkHours()
+                }
+            },
+            vote_maybe: {
+                name: '(maybe)',
+                callback(key, selection, clickEvent) {
+                    setSelectedCells('')
+                }
+            },
+            vote_no: {
+                name: 'no',
+                callback(key, selection, clickEvent) {
+                    setSelectedCells('no')
+                }
+            },
+        },
     },
     outsideClickDeselects: false,
     selectionMode: 'multiple',
     licenseKey: 'non-commercial-and-evaluation',
-});
+})
+
+
+$("#btn-more-days").click(function () {
+    loadMoreData(7)
+    hot.scrollViewportTo(hot.countRows() - 1)
+})
 
 $("#btn-answer-ok").click(function () {
-    const selected = hot.getSelected() || [];
-    setSelectedCells(selected, 'ok');
+    setSelectedCells('ok')
 })
 
 $("#btn-answer-maybe").click(function () {
-    const selected = hot.getSelected() || [];
-    setSelectedCells(selected, '');
+    setSelectedCells('')
 })
 
 $("#btn-answer-no").click(function () {
-    const selected = hot.getSelected() || [];
-    setSelectedCells(selected, 'no');
+    setSelectedCells('no')
 })
 
 $("#btn-answer-hours").click(function () {
-    const selected = hot.getSelected() || [];
-    value = $(`#input-answer-hours`).val()
-    setSelectedCells(selected, value);
+    voteOkHours()
 })
 
-function setSelectedCells(selected, value) {
-    hot.suspendRender();
-    console.log('setting ok', selected)
+function voteOkHours() {
+    value = $(`#input-answer-hours`).val()
+    setSelectedCells(value)
+}
+
+function setSelectedCells(value) {
+    const selected = hot.getSelected() || []
+    hot.suspendRender()
 
     for (let index = 0; index < selected.length; index += 1) {
-      const [row1, column1, row2, column2] = selected[index];
-      const startRow = Math.max(Math.min(row1, row2), 0);
-      const endRow = Math.max(row1, row2);
-      const startCol = Math.max(Math.min(column1, column2), 0);
-      const endCol = Math.max(column1, column2);
-  
-      for (let rowIndex = startRow; rowIndex <= endRow; rowIndex += 1) {
-        for (let columnIndex = startCol; columnIndex <= endCol; columnIndex += 1) {
-            hot.setDataAtCell(rowIndex, columnIndex, value);
+        const [row1, column1, row2, column2] = selected[index]
+        const startRow = Math.max(Math.min(row1, row2), 1)
+        const endRow = Math.max(row1, row2)
+        const startCol = Math.max(Math.min(column1, column2), 1)
+        const endCol = Math.max(column1, column2)
+
+        for (let rowIndex = startRow; rowIndex <= endRow; rowIndex += 1) {
+            for (let columnIndex = startCol; columnIndex <= endCol; columnIndex += 1) {
+                hot.setDataAtCell(rowIndex, columnIndex, value)
+            }
         }
-      }
     }
   
-    hot.render();
-    hot.resumeRender();
+    hot.render()
+    hot.resumeRender()
 }
